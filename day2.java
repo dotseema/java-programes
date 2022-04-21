ffbf971c4963dfe5de415e296c65291a4e883aaf
dotseema <99743625+dotseema@users.noreply.github.com>
2022-04-21 23:06:21 +0530

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,16 @@
+public class day2 {
+
+public static void main(String[] args){
+
+int age =40;
+String name = "tom";
+char gender = 'm';
+double salary = 654321;
+boolean isactive = true;
+System.out.println("hello world ");
+System.out.println("details of employee");
+System.out.println(name + "  " + gender + "  " + age + "  " + salary + "  " + isactive);
+
+}
+
+}
---
+++ narrowingcasting.java
@@ -0,0 +1,12 @@
+public class narrowingcasting{
+
+public static void main(String[] args){
+
+double myDouble = 9.87d;
+int myInt = (int) myDouble;
+System.out.println(myDouble);
+System.out.println(myInt);
+
+}
+
+}
