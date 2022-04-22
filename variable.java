c52500be0146e74ded03605bc31b3f18464a90fb
dotseema <99743625+dotseema@users.noreply.github.com>
2022-04-22 20:07:33 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,17 @@
+public class variable {
+
+public static void main(String[] args) {
+
+int myInt = 56;
+float myFloat = 12.5f;
+char myletter = 'A';
+boolean myBool = true;
+String name = "seema";
+System.out.println(myInt);
+System.out.println(myFloat);
+System.out.println(myletter);
+System.out.println(myBool);
+System.out.println(name);
+
+}
+}
